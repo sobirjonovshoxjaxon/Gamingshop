aa10a016121fbe9e31df6adf134c88a0735a3dc2
shoxjaxon <shohjahonsobirjonov76@gmail.com>
2024-12-08 14:18:18 +0500

    men tugatdim 100% tugadi faqat js faylida ozgina muomo bor bo'ldi

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// start header scroll
 window.onscroll = function() {
     var header = document.getElementById("header");
     var navbar = document.getElementById("navbar");
@@ -26,3 +27,57 @@ document.querySelectorAll('input[type="radio"]').forEach(input => {
 
 // Birinchi input tanlangan holatda bo'lishi uchun
 document.querySelector('input[type="radio"]:checked').dispatchEvent(new Event('change'));
+
+// end header scroll
+
+
+
+// start tab
+
+let btn 
+let text 
+
+window.onload = function(){
+
+    btn = document.querySelectorAll("#tab .btn")
+    text = document.querySelectorAll("#tab .tab-text")
+
+    texthidden()
+
+    text[0].classList.add("show")
+    text[0].classList.remove("hide")
+    btn[0].classList.add("active")
+}
+
+
+function texthidden(){
+
+    for(let i=0; i<btn.length; i++){
+
+        btn[i].classList.remove("active")
+        text[i].classList.add("hide")
+    }
+}
+
+document.getElementById("tab").onclick=function(e){
+
+    let t = e.target 
+    console.log(t)
+
+    for(let i=0; i<btn.length; i++){
+
+       if(btn[i] == t){
+
+        texthidden()
+
+        btn[i].classList.add("active")
+        text[i].classList.add("show")
+        text[i].classList.remove("hide")
+       }else{
+
+        text[i].classList.remove("show")
+       }
+    }
+}
+
+// end tab
